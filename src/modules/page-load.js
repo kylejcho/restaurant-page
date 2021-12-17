969f62f29376d17dbddaed97253eef45081420d7
Kyle Cho <kylejcho@gmail.com>
2021-12-17 16:57:14 -0500

    Homepage background and nav change

--- src/modules/page-load.js
+++ src/modules/page-load.js
@@ -9,7 +9,7 @@ const createNav = () => {
 const createHeader = (nav) => {
     const header = document.createElement("header");
     header.id = "pageTitle"
-    header.innerText = "Restaurant";
+    header.innerText = "KYLE'S KITCHEN";
     nav.append(header);
 }
 
@@ -24,13 +24,34 @@ const createTabButtonContainer = (nav) => {
 
 const createTabButton = (name, container) => {
     const button = document.createElement("button");
-    button.id = name;
-    button.innerText = name;
+    button.id = name + "Button";
+    button.innerText = name.toUpperCase();
     container.append(button);
 }
 
+const createHomeContent = () => {
+    const homeContent = document.createElement("div");
+    homeContent.id = "homeContent";
+    document.querySelector("#content").append(homeContent);
+    const title = document.createElement("header");
+    title.innerText = "ENJOY FOOD FROM KYLE'S KITCHEN.";
+    homeContent.append(title);
+    const subTitle = document.createElement("p");
+    subTitle.innerText = "We a great selection of food from all around the world. Enjoy every taste that this earth has to offer!"
+    homeContent.append(subTitle);
+}
+
+const createFooter = () => {
+    const footer = document.createElement("footer");
+    document.querySelector("#content").append(footer);
+}
+
+
+
 function loadPage() {
     createNav();
+    createHomeContent();
+    createFooter();
 }
 
 
