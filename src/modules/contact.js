f79b4ca06249cf4f1d12d1aee522fa817bea881c
Kyle Cho <kylejcho@gmail.com>
2021-12-18 13:50:11 -0500

    Contact container

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -3,6 +3,15 @@ const createContactContent = () => {
     contactContentContainer.id = "contactContentContainer";
     let content = document.querySelector("#content");
     content.children.item(1).replaceWith(contactContentContainer);
+
+    const contactContainer = document.createElement('div');
+    contactContainer.id = "contactContainer";
+    contactContentContainer.append(contactContainer);
+
+    const header= document.createElement('header');
+    header.id = "contactHeader";
+    header.innerText = "CONTACT"
+    contactContainer.append(contact);
 }
 
 export default createContactContent;
