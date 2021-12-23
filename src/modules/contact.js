6506129d45c0fb636b0fe7e694cf951132bc2d18
Kyle Cho <kylejcho@gmail.com>
2021-12-23 16:07:49 -0500

    Contact page

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -4,29 +4,83 @@ const createContactContent = () => {
     let content = document.querySelector("#content");
     content.children.item(1).replaceWith(contactContentContainer);
 
+    const contactContainerBackground = document.createElement('div');
+    contactContainerBackground.id = "contactContainerBackground";
+    contactContentContainer.append(contactContainerBackground);
+
     const contactContainer = document.createElement('div');
     contactContainer.id = "contactContainer";
-    contactContentContainer.append(contactContainer);
+    contactContainerBackground.append(contactContainer);
 
     const header= document.createElement('header');
     header.id = "contactHeader";
     header.innerText = "CONTACT"
     contactContainer.append(header);
 
+    const contactMapContainer = document.createElement('div');
+    contactMapContainer.id = "contactMapContainer";
+    contactContainer.append(contactMapContainer);
+    const a = document.createElement('p');
+    a.innerText = ("KYLE'S");
+    a.id = "a"
+    contactMapContainer.append(a);
+    const b = document.createElement('p');
+    b.innerText = ("KITCHEN");
+    b.id = "b"
+    contactMapContainer.append(b)
+
     const contactInfoContainer = document.createElement('div');
     contactInfoContainer.id = "contactInfoContainer";
     contactContainer.append(contactInfoContainer);
 
-    const contactMapContainer = document.createElement('div');
-    contactMapContainer.id = "contactMapContainer";
-    contactContainer.append(contactMapContainer);
+    createContactElement("addressHeader", "KYLE'S KITCHEN");
+    createContactElement("address", "66 Silver Spear St.\nIndianapolis, IN 46201");
+    createContactElement("phone", "(240)-525-6834");
+    createContactElement("email", "kyleskitchen@restaurant.com");
 
-    for (let i = 0; i < 5; i++) {
-        const div = document.createElement('p');
-        contactInfoContainer.append(div)
-    }   
+    createHoursContainer((contactInfoContainer));
+}
+
+const createContactElement = (id, content) => {
+    const div = document.createElement('p');
+    div.id = id;
+    div.innerText = content;
+    contactInfoContainer.append(div)
+}
+
+const createHoursContainer = (contactInfoContainer) => {
+    const hoursContainer = document.createElement('div');
+    hoursContainer.id = "hoursContainer";
+    contactInfoContainer.append(hoursContainer);
+
+    const hoursHeader = document.createElement('p');
+    hoursHeader.id = "hoursHeader";
+    hoursHeader.innerText = "HOURS";
+    hoursContainer.append(hoursHeader);
+
+    createHoursContent("Monday - Wednesday:", "10AM - 12PM", hoursContainer);
+    createHoursContent("Thurday - Friday:", "9AM - 12PM", hoursContainer);
+    createHoursContent("Saturday:", "1PM - 12PM", hoursContainer);
+    createHoursContent("Sunday:", "1PM - 10PM", hoursContainer);
 
 
 }
+
+const createHoursContent = (dayRange, hourRange, hoursContainer) => {
+    const hoursContentContainer = document.createElement('div');
+    hoursContentContainer.id = "hoursContentContainer";
+    hoursContainer.append(hoursContentContainer);
+
+    const days = document.createElement('p');
+    days.id = "days";
+    days.innerText = dayRange;
+    hoursContentContainer.append(days);
+
+    const hours = document.createElement('p');
+    hours.id = "hours";
+    hours.innerText = hourRange;
+    hoursContentContainer.append(hours);
+}
+
 
 export default createContactContent;
